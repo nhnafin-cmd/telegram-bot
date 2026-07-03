93ef9400b5c6728f7f06c77e81305513fa6ccc29
nhnafin-cmd <nhnafin@gmail.com>
2026-07-04 02:53:57 +0600

    Update bot.py to use BOT_TOKEN and streamline start
    Refactor bot.py to use environment variable for token and simplify start command response.

--- bot.py
+++ bot.py
@@ -1,30 +1,13 @@
-import os
-from telegram import Update, ReplyKeyboardMarkup
+from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
+import os
 
-TOKEN = os.getenv(8812541901:AAF12HU1Gp2A9arTLaS_manzrS1euql2q4c)
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [
-        ["📋 কাজ ▸"],
-        ["💰 ব্যালেন্স", "💸 টাকা উত্তোলন"],
-        ["👥 My Referrals"],
-        ["🛠️ সাপোর্ট", "🆕 আমি নতুন"]
-    ]
-
-    reply_markup = ReplyKeyboardMarkup(
-        keyboard,
-        resize_keyboard=True
-    )
-
-    await update.message.reply_text(
-        "🎉 WELCOME TO OFFICIAL INSTAGRAM SELL BD BOT\n\n"
-        "নিচের মেনু থেকে একটি অপশন নির্বাচন করুন।",
-        reply_markup=reply_markup
-    )
+    await update.message.reply_text("🤖 WELCOME OUR OFFICIAL INSTAGRAM BOT BD")
 
 app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
-    
